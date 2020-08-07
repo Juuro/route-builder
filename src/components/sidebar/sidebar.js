34477e7f6235f2a47aa29e6bf1a8dfb21f6cc902
Juuro <mail@sebastian-engel.de>
2020-08-07 12:09:35 +0200

    Format document.

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,19 +1,25 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 import Waypoint from '../waypoint/waypoint'
 
 import './sidebar.css'
 
 const Sidebar = () => {
-  const [waypoints, setWaypoints] = useState([{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}])
+  const [waypoints, setWaypoints] = useState([
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 },
+  ])
   const [dragEl, setDragEl] = useState(null)
 
-  const moveWaypoint = el => {
-    setWaypoints(prevState => {
-      const itemIndex = prevState.findIndex(i => {
+  const moveWaypoint = (el) => {
+    setWaypoints((prevState) => {
+      const itemIndex = prevState.findIndex((i) => {
         return i.id === dragEl.id
       })
-      const hoverIndex = prevState.findIndex(i => {
+      const hoverIndex = prevState.findIndex((i) => {
         return i.id === el
       })
       const newstate = [...prevState]
@@ -25,7 +31,7 @@ const Sidebar = () => {
     })
   }
 
-  const setDragElement = el => {
+  const setDragElement = (el) => {
     setDragEl(el)
   }
 
@@ -33,13 +39,17 @@ const Sidebar = () => {
     <aside className="sidebar">
       <h1>Route Builder</h1>
       <hr />
-        <div className="waypoints">
-
-          {waypoints.map(waypoint => (
-            <Waypoint key={waypoint.id} id={waypoint.id} waypoint={waypoint} moveWaypoint={moveWaypoint} setDragElement={setDragElement} />
-          ))}
-          
-        </div>
+      <div className="waypoints">
+        {waypoints.map((waypoint) => (
+          <Waypoint
+            key={waypoint.id}
+            id={waypoint.id}
+            waypoint={waypoint}
+            moveWaypoint={moveWaypoint}
+            setDragElement={setDragElement}
+          />
+        ))}
+      </div>
       <button>
         <span>Download your Route</span>
       </button>
--- src/components/waypoint/waypoint.js
+++ src/components/waypoint/waypoint.js
@@ -1,34 +1,40 @@
 import React from 'react'
 
-import {ReactComponent as DeleteIcon} from './delete.svg'
+import { ReactComponent as DeleteIcon } from './delete.svg'
 
 import './waypoint.css'
 
-const Waypoint = ({waypoint, moveWaypoint, setDragElement, id}) => {
-  const removeWaypoint = event => {
+const Waypoint = ({ waypoint, moveWaypoint, setDragElement, id }) => {
+  const removeWaypoint = (event) => {
     console.log('removeWaypoint')
   }
 
-  const onDragStart = ({target}) => {
+  const onDragStart = ({ target }) => {
     setDragElement(waypoint)
     setTimeout(() => {
-      target.style.opacity = .5
+      target.style.opacity = 0.5
       target.classList.add('dragging')
     }, 1)
   }
 
-  const onDragOver = id => event=> {
+  const onDragOver = (id) => (event) => {
     moveWaypoint(id)
     event.preventDefault()
   }
 
-  const onDragEnd = ({target}) => {
+  const onDragEnd = ({ target }) => {
     target.style.opacity = 1
     target.classList.remove('dragging')
   }
 
   return (
-    <div className="waypoint" draggable="true" onDragStart={onDragStart} onDragOver={onDragOver(id)} onDragEnd={onDragEnd}>
+    <div
+      className="waypoint"
+      draggable="true"
+      onDragStart={onDragStart}
+      onDragOver={onDragOver(id)}
+      onDragEnd={onDragEnd}
+    >
       <div className="waypoint-drag-handle">
         <span></span>
         <span></span>
