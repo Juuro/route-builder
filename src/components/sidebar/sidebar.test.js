1972b2e97412e98ccb063fab83650fe887c1d1e8
Juuro <mail@sebastian-engel.de>
2020-08-08 00:15:18 +0200

    Setup tests.

---
+++ src/components/sidebar/sidebar.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './sidebar';
+
+describe('Sidebar', () => {
+  it('renders heading', () => {
+    const { getByText } = render(<App />)
+    const linkElement = getByText(/Route Builder/i)
+    expect(linkElement).toBeInTheDocument()
+  })
+})
