7b8c9ca579ed1a53403e5b9d721ff31b44bfef88
Juuro <mail@sebastian-engel.de>
2020-08-09 21:10:37 +0200

    Reducer tests.

---
+++ src/redux/reducers/reducers.test.js
@@ -0,0 +1,80 @@
+import reducer from './reducers'
+
+describe('reducer', () => {
+    const INITIAL_STORE = {
+        map: {},
+        markers: [],
+    }
+
+    const markers = [
+        {id: 1, marker: {}},
+        {id: 2, marker: {}},
+        {id: 3, marker: {}},
+        {id: 4, marker: {}},
+    ]
+
+    afterEach(() => {
+        jest.clearAllMocks()
+    })
+
+    it('with action type ADD_MARKERS should add marker to store', () => {
+        const marker = {id: 5, marker: {}}
+        const action = {type: 'ADD_MARKERS', payload: marker}
+
+        const newState = reducer(INITIAL_STORE, action)
+
+        expect(newState).toEqual({
+            ...INITIAL_STORE,
+            markers: [marker],
+        })
+    })
+
+    it('with action type REPLACE_MARKERS should replace all markers to store', () => {
+        const marker = {id: 5, marker: {}}
+        const action = {type: 'REPLACE_MARKERS', payload: [marker]}
+
+        const newState = reducer(INITIAL_STORE, action)
+
+        expect(newState).toEqual({
+            ...INITIAL_STORE,
+            markers: [marker],
+        })
+    })
+
+    it('with action type REMOVE_MARKER should a marker from the store', () => {
+        const action = {type: 'REMOVE_MARKER', payload: 2}
+
+        const newMarkers = [
+            {id: 1, marker: {}},
+            {id: 3, marker: {}},
+            {id: 4, marker: {}},
+        ]
+
+        const newState = reducer({...INITIAL_STORE, markers}, action)
+
+        expect(newState).toEqual({
+            ...INITIAL_STORE,
+            markers: newMarkers,
+        })
+    })
+
+    it('with action type ADD_MAP should add the map to the store', () => {
+        const newMap = {this: 'is the map object'}
+        const action = {type: 'ADD_MAP', payload: newMap}
+
+        const newState = reducer(INITIAL_STORE, action)
+
+        expect(newState).toEqual({
+            ...INITIAL_STORE,
+            map: newMap,
+        })
+    })
+
+    it('with no state and unknown action should return the initial state', () => {
+        const action = {type: 'UNKNOWN'}
+
+        const newState = reducer(undefined, action)
+
+        expect(newState).toEqual(INITIAL_STORE)
+    })
+})
