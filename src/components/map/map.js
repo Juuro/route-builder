8488450c309c3911235eb3d6f20374da8e947be8
Juuro <mail@sebastian-engel.de>
2020-08-10 01:31:02 +0200

    Enable redux-logger only in development mode.

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -35,14 +35,12 @@ const Map = () => {
         dispatch({type: 'ADD_MAP', payload: map.current})
 
         Leaflet.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-            attribution:
-          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
             maxZoom: 18,
             id: 'mapbox/outdoors-v11',
             tileSize: 512,
             zoomOffset: -1,
-            accessToken:
-          'pk.eyJ1IjoianV1cm8iLCJhIjoiY2tkaGdoNzk0MDJ1YTJzb2V4anZ3NXk4bSJ9.1m7LQQaTf2W4R-IgKKGZCQ',
+            accessToken: 'pk.eyJ1IjoianV1cm8iLCJhIjoiY2tkaGdoNzk0MDJ1YTJzb2V4anZ3NXk4bSJ9.1m7LQQaTf2W4R-IgKKGZCQ',
         }).addTo(map.current)
 
         map.current.on('click', onMapClick)
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,11 @@ import {applyMiddleware, createStore} from 'redux'
 import logger from 'redux-logger'
 import rootReducer from './reducers/reducers'
 
-const store = createStore(rootReducer, applyMiddleware(logger))
+const middleware = []
+if (process.env.NODE_ENV === 'development') {
+    middleware.push(logger)
+}
+
+const store = createStore(rootReducer, applyMiddleware(...middleware))
 
 export default store
